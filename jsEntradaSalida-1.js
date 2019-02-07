f682e4afe46105e401d75d8135195502cc6dcf7f
victoriaceronutn <47418185+victoriaceronutn@users.noreply.github.com>
2019-02-07 12:10:36 -0300

    Add files via upload

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,21 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+function mostrar()
+{
+// esto es un comentario
+	/*
+	// esto es un comentario 
+de
+varias
+lineas
+*/ 
+alert("Esto funciona de maravilla");
+dato=prompt("dato")
+var dato;
+dato = document.getElementById("elNombre").Value;
+console.log(dato);
+console.info(dato)
+console.error(dato);
+
+}
+
+
