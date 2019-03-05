7b0e9f8f8c9a3ef9f5a275198ec0330e9d3bd7fe
victoriaceronutn <victoria_ceron@outlook.com>
2019-03-05 20:02:29 -0300

    parcial3

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/ocho.js
@@ -1,4 +1,93 @@
 function mostrar()
 {
+    var pais;
+    var territorio;
+    var contador;
+    var respuesta;
+    
+    var superficieImpar;
+    var superficeMenorCien;
+    var superficeIgualCien;
+
+    var contadorPais;
+    var primerPais;
+    var contadorKm;
+    var sumaKm;
+    var promedioKm;
+    var territorioMin;
+    var paisMin;
+    
+    contador =0;
+    respuesta ="si";
+
+    superficieImpar =0;
+    superficeMenorCien =0;
+    superficeIgualCien =0;
+
+    contadorPais =0;
+    contadorKm =0;
+    sumaKm =0;
+
+    while(respuesta!= "no")
+    {
+        pais=prompt("Ingrese un pais");
+
+        territorio=prompt("Ingrese un territorio");
+        territorio=parseInt(territorio);
+
+        while(territorio<1)
+        {
+            territorio=prompt("Reingrese el territorio");
+
+        }
+        if(territorio%2!=0) /* %2 = es para saber si es par */
+        {                   /* %2! = para saber numero impar */
+            superficieImpar++;
+        }
+        if(territorio<100)
+        {
+            superficeMenorCien++;
+        }
+        if(territorio==100)
+        {
+            superficeIgualCien++;
+        }
+        if(territorio>100)
+        {
+            contadorPais++;
+
+            if(contadorPais==1)
+            {
+                primerPais=pais;
+            }
+        }
+        if(contador=0)
+        {
+            territorioMin=territorio;
+            paisMin=pais;
+        }
+        else
+        {
+            if(territorio<territorioMin)
+            {
+                territorioMin=territorio;
+                paisMin=pais;
+            }
+        }
+        contador++;
+        contadorKm++;
+        sumaKm=sumaKm+territorio;
+        respuesta=prompt("Desea seguir?");
+
+    }
+
+promedioKm=sumaKm/contadorKm;
+
+    document.write("La cantidad de paises con superficie impar : " + superficieImpar );
+    document.write("La cantidad de paises con superficie menor a 100 : " + superficeMenorCien);
+    document.write("La cantidad de paises con superficie igual a 100 : " + superficeIgualCien);
+    document.write("El nombre del primer pais que supero los 100 de superficie : " + primerPais);
+    document.write("El promedio de km ingresados : " + promedioKm );
+    document.write("El nombre del que menos territorio tiene : " + paisMin);
 
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/siete.js
@@ -1,4 +1,76 @@
 function mostrar()
 {
+    var velocidad;
+    var combustible;
+    var contador;
+    var promedio;
+    var sumaDeVelocidades;
+    var velocidadMinima;
+    var combustibleVelocidadMasBaja;
+    var contadorCombustible;
+    var veocidadMaximaSolido;
+
+    velocidad=0;
+    combustible=0;
+    contador=0;
+    sumaDeVelocidades=0;
+    velocidadMinima=0;
+    combustibleVelocidadMasBaja=0;
+    contadorCombustible=0;
+    veocidadMaximaSolido=0;
+
+    while(contador<5)
+    {
+        velocidad=prompt("Ingrese una velocidad en km");
+        velocidad=parseInt(velocidad);
+
+        while(velocidad<0 || velocidad>250)
+        {
+            velocidad=prompt("Error, ingrese de nuevo la velocidad");
+            break;
+        }
+        combustible=prompt("Ingrese el tipo de combustible");
+        
+        while(combustible !="liquido" || combustible !="solido")
+        {
+            combustible=prompt("Error, ingrese si es solido o liquido");
+            break;
+        }
+        sumaDeVelocidades=sumaDeVelocidades+velocidad;
+
+        if(velocidad<=velocidadMinima || contador==0)
+        {
+            velocidadMinima=velocidad;
+            combustibleVelocidadMasBaja=combustible;
+        }
+        else
+        {
+            if(velocidad<velocidadMinima)
+            {
+                velocidadMinima=velocidad;
+                combustibleVelocidadMasBaja=combustible;
+            }
+        }
+        if(velocidad>100 && combustible=="liquido")
+        {
+            contadorCombustible++;
+        }
+        if(velocidad>veocidadMaximaSolido && combustible=="solido")
+        {
+            veocidadMaximaSolido=velocidad;
+        }
+
+    contador++;
+    velocidadMinima++;
+    veocidadMaximaSolido++;   
+    }
+
+promedio=sumaDeVelocidades/5;
+
+alert("El promedio de las velocidades es " + promedio );
+alert("La velocidad minima es " + velocidadMinima + " y el combustible es " + combustibleVelocidadMasBaja );
+alert("Cantidad de combustible liquido : " + contadorCombustible );
+alert("La velocidad mas alta de los solidos es " + veocidadMaximaSolido );
+
 
 }
